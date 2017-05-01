065fea1044e6123ac25085ebf19ec39afdff5e84
Cokemonkey11 <Cokemonkey11@gmail.com>
2017-05-01 21:28:40 +0100

    use gradle

--- src/main/scala/example/MpqDiff.scala
+++ src/main/scala/example/MpqDiff.scala
@@ -1,25 +1,31 @@
-package example
+package de.inwc.mpqdiff
 
 import java.io._
 
 import systems.crigges.jmpq3.JMpqEditor
 
 object MpqDiff extends App {
-  val editors = args.map { arg: String =>
-    println(arg);
-    new JMpqEditor(new File(arg))
+  println(s"\n  Begin, with arguments [%s].".format(args.mkString(", ")))
+
+  if (args.length != 2) {
+    println("  Invalid arguments. See README.md for usage.")
+  } else {
+    val editors = args.map { arg: String =>
+      println(s"  $arg");
+      new JMpqEditor(new File(arg))
+    }
+
+    val temp_listings = editors.zipWithIndex.map { case (editor: JMpqEditor, index: Int) =>
+      println(s"  $editor");
+      println(s"  $index");
+
+      val f = new File("temp-" + index);
+      f.mkdir;
+      editor.extractAllFiles(f);
+
+      f.listFiles
+    }
+
+    println(s"  %s".format(temp_listings.transpose.head.head.getName))
   }
-
-  val temp_listings = editors.zipWithIndex.map { case (editor: JMpqEditor, index: Int) =>
-    println(editor);
-    println(index);
-
-    val f = new File("temp-" + index);
-    f.mkdir;
-    editor.extractAllFiles(f);
-
-    f.listFiles
-  }
-
-  println(temp_listings.transpose.head.head.getName)
 }
